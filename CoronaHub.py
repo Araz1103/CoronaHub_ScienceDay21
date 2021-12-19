8b3a614d44fe51a2e6f393e74668de2fa10d7eef
Araz1103 <arazsharma1103@gmail.com>
2021-12-19 12:19:14 +0530

    Updated Repo

--- CoronaHub.py
+++ CoronaHub.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pydeck as pdk
 import datetime #To use with Dates
 import matplotlib.pyplot as plt #To help to plot
 from PIL import Image
